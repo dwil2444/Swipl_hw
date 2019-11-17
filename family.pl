8ce98fc6d0ac0fdcf052566c684772163a1cd3c7
dwil2444 <daneandrew16@gmail.com>
2019-11-16 21:46:28 -0500

    completed mother in law procedure

--- family.pl
+++ family.pl
@@ -1,31 +1,57 @@
+/**
+ *  Dane A. Williamson
+ *  CSCI 485 - 01
+ *  11/16/19
+ *  Assignment 5
+ **/
+
+
+
 father(abraham, isaac).
 father(noah, ham).
 father(thomas, bruce).
 father(abraham, ishmael).
-father(isaac, jacob)
+father(isaac, jacob).
 
+mother(sarah, bethuel).
+
+husband(ishmael, bethuel).
+wife(bethuel, ishmael).
+
+parent(X, Y) :- father(X,Y).
+parent(X, Y) :- mother(X,Y).
 grandfather(X, Y) :- father(X, Z), parent(Z, Y).
+
+married(X, Y) :- husband(X, Y), wife(Y, X).
+
+mother_in_law(X, Y) :- mother(X, Z), married(Y, Z).
 
 
 /**
  * 1. Query to list all sons of Abraham:
- * father(abraham, X).
- * type semicolon to see further answers
+ *    father(abraham, X).
+ *    type semicolon to see further answers
  **/
 
 
  /**
- * 2. Query to list all fathers of Isaac:
- * father(Y, isaac).
+ * 2. Query to view Isaac's father:
+ *    father(Y, isaac).
  **/
 
   /**
  * 3. Query to list the combinations of fathers and sons:
- * father (X, Y).
+ *    father (X, Y).
  * type semicolon to see all answers
  **/
 
- /**
- * 4. 
- * father(Y, isaac).
+  /**
+ * 4. Query to check if abraham is jacob's grandfather
+ *    grandfather(abraham, ishmael).
+ **/
+
+
+ 
+/**
+ * Extra Credit: write prolog rule to deduce a mother in law relationship
  **/
